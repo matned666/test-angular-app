8c255c50f27c04d52259a46c976220d16e53d9ec
MNiedbal <mniedbal@sygnity.pl>
2024-11-13 12:44:36 +0100

    using output function - modern approach

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, output} from '@angular/core';
 
 @Component({
   selector: 'app-user-button',
@@ -12,7 +12,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   get userImagePath() {
     return 'assets/users/' + this.avatar;
