793070ec6c4e8998ebb7d35d0765d7ae694c85a5
MNiedbal <mniedbal@sygnity.pl>
2024-11-14 09:47:26 +0100

    ZAJEBISTE - pobieranie wartości z textu za pomocą twoWay binding

--- src/app/user-tasks/add-new-user-task/add-new-user-task.component.ts
+++ src/app/user-tasks/add-new-user-task/add-new-user-task.component.ts
@@ -1,38 +1,37 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TaskModel} from '../user-task/task.model';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-add-new-user-task',
   templateUrl: './add-new-user-task.component.html',
   styleUrls: ['./add-new-user-task.component.css'],
+  imports: [
+    FormsModule
+  ],
   standalone: true
 })
 export class AddNewUserTaskComponent {
   @Input({required:true}) userId?: string;
-  @Output() close = new EventEmitter();
+  @Output() close = new EventEmitter<void>();
   @Output() save = new EventEmitter<TaskModel>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancelButtonClick() {
     this.close.emit();
   }
 
-  onSaveButtonClick(title: string, summary: string, dueDate: string) {
-
-    // tworzenie aktualnej daty w typescript
-    //
-    // const today = new Date();
-    // const year = today.getFullYear();
-    // const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Dodaj 1, ponieważ miesiące są indeksowane od 0
-    // const day = today.getDate().toString().padStart(2, '0');
-    // const dueDate = `${year}-${month}-${day}`;
-
+  onSaveButtonClick() {
     const newTask : TaskModel = {
       id: '0',
       userId: this.userId ?? '',
-      title: title,
-      summary: summary,
-      dueDate: dueDate
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDate
     };
 
     console.log(newTask);
