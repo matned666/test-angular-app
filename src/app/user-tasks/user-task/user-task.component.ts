843c2032fbc51fc9f68eb8ac2466c743cc1c88d4
MNiedbal <mniedbal@sygnity.pl>
2024-11-14 10:47:22 +0100

    ngcontent - wrapowanie elementów w inne elementy

--- src/app/user-tasks/user-task/user-task.component.ts
+++ src/app/user-tasks/user-task/user-task.component.ts
@@ -1,10 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TaskModel} from './task.model';
+import {CardComponent} from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-user-task',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './user-task.component.html',
   styleUrl: './user-task.component.css'
 })
